7325be042aedb56774948be4210c059b1f7df834
tanakashunta0801 <yoro46@icloud.com>
2021-07-10 02:16:48 +0900

    Initial Commit

---
+++ GithubTestApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubTestApp
+//
+//  Created by 田中俊多 on 2021/07/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubTestApp/GithubTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestAppApp.swift
+//  GithubTestApp
+//
+//  Created by 田中俊多 on 2021/07/10.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
